24312fbbef421c50a99b0e250f63fafc6f79a05f
Toteya <111172685+Toteya@users.noreply.github.com>
2025-05-22 13:25:43 +0200

    simple REST API Flask app

---
+++ server/api/v1/app.py
@@ -0,0 +1,26 @@
+"""
+REST API Flask app
+"""
+from flask import Flask, jsonify
+
+
+def create_app():
+    """ Create a Flask app
+    """
+    app = Flask(__name__)
+    # app.register_blueprint(app_views, url_prefix='/api/v1')
+    return app
+
+
+app = create_app()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Handle 404 errors
+    """
+    return jsonify({'error': error or 'Not Found'}), 404
+
+
+if __name__ == '__main__':
+    app.run(port='5001', host='0.0.0.0')
