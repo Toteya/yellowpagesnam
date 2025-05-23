60c6a0e8ef2523204f4bfa3b943d045f6978b3f2
Toteya <111172685+Toteya@users.noreply.github.com>
2025-05-23 18:26:38 +0200

    test to_dict() and save() methods

--- server/tests/test_models/test_base.py
+++ server/tests/test_models/test_base.py
@@ -1,11 +1,12 @@
 """
 base model unit tests
 """
-from unittest import TestCase
+import unittest
 from server.models.base import BaseModel
+from server.models.listing import Listing
 
 
-class Test_Base(TestCase):
+class Test_Base(unittest.TestCase):
     """ Test the Base Model
     """
     def test_base(self):
@@ -13,3 +14,29 @@ class Test_Base(TestCase):
         """
         b1 = BaseModel()
         self.assertTrue(isinstance(b1, BaseModel))
+
+    def test_to_dict(self):
+        """ Test the to_dict method
+        """
+        listing1 = Listing(name="my_listing", category="my_category")
+        listing1.save()
+
+        expected_dict_subset = {'name': 'my_listing', '__class__': 'Listing'}
+        
+        for key, value in expected_dict_subset.items():
+            self.assertIn(key, listing1.to_dict())
+            self.assertEqual(listing1.to_dict().get(key), value)
+    
+    def test_save(self):
+        """ Test the save method
+        """
+        listing1 = Listing(name="my_listing", category="my_category")
+        listing1.save()
+        self.assertEqual(listing1.created_at, listing1.updated_at)
+        
+        # Update the listing
+        listing1.name = "updated_listing"
+        listing1.save()
+        
+        # Check that updated_at is different from created_at
+        self.assertNotEqual(listing1.created_at, listing1.updated_at)
