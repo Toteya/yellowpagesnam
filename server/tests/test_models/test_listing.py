93fe736f4694632c2068670cc9145f0562ae9e2d
Toteya <111172685+Toteya@users.noreply.github.com>
2025-05-23 17:22:40 +0200

    updated tests to be consistent with mongoengine ODM changes

--- server/tests/test_models/test_listing.py
+++ server/tests/test_models/test_listing.py
@@ -11,5 +11,6 @@ class TestListing(TestCase):
     def test_listing(self):
         """ Test that the instance initialises correctly
         """
-        listing1 = Listing()
+        listing1 = Listing(name="my_listing", category="my_category")
+        listing1.save()
         self.assertEqual(listing1.created_at, listing1.updated_at)
