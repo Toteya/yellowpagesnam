7447edda59077ade2a91c103d9cbb3506c2901f6
Toteya <111172685+Toteya@users.noreply.github.com>
2025-05-24 12:51:54 +0200

    listings endpoints: get all listings, get one listing based on ID

--- server/api/v1/views/listings.py
+++ server/api/v1/views/listings.py
@@ -3,6 +3,8 @@ REST API endpoints for listings
 """
 from flask import abort, jsonify, request
 from server.api.v1.views import app_views
+from server.models import storage
+from server.models.listing import Listing
 
 
 @app_views.route('/listings', methods=['GET'], strict_slashes=False)
@@ -10,5 +12,16 @@ def get_listings():
     """
     Get all listings
     """
-    listings = []
+    listings = storage.all(Listing)
+    listings = [listing.to_dict() for listing in listings]
     return jsonify(listings), 200
+
+@app_views.route('/listings/<listing_id>', strict_slashes=False)
+def get_listing(listing_id):
+    """
+    Get a specific listing by ID
+    """
+    listing = storage.get(Listing, listing_id)
+    if not listing:
+        abort(404, description="Listing not found")
+    return jsonify(listing.to_dict()), 200
