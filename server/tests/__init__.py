7faf53a5f1f0d651215795b386825df6c7b8df01
Toteya <111172685+Toteya@users.noreply.github.com>
2025-05-25 15:47:56 +0200

    setup testing environment when 'test' package initialises

---
+++ server/tests/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+# Setting environment variables
+os.environ['DIRECTORY_HOST'] = 'localhost'
+os.environ['DIRECTORY_PORT'] = '27017'
+os.environ['DIRECTORY_DB'] = 'directoy_db_test'
+os.environ['DIRECTORY_ENV'] = 'testing'
