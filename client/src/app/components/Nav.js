8a65d11cf1c502fd21ef365b9be1078e4f0c9d52
Toteya <111172685+Toteya@users.noreply.github.com>
2025-06-01 15:23:52 +0200

    updated styling: sticky navbar (sticks to the top when scrolling)

--- client/src/app/components/Nav.js
+++ client/src/app/components/Nav.js
@@ -1,18 +1,16 @@
-import './Nav.module.css';
-// import { Link } from 'react-router-dom';
 import Link from 'next/link';
 
 const Nav = () => {
   return (
-    <nav className="w-full bg-slate-800 flex flex-col justify-start items-start">
-      <ul className="text-white list-none flex flex-nowrap items-center m-0">
-        <li className="group p-4 hover:bg-gray-200 hover:text-gray-800 focus:bg-gray-200 focus:text-gray-800 cursor-pointer">
+    <nav className="w-full bg-slate-800 text-white flex items-center justify-between sticky top-0 z-50">
+      <ul className="flex space-x-2 pl-2">
+        <li className="group p-2 hover:bg-gray-200 hover:text-gray-800 focus:bg-gray-200 focus:text-gray-800 cursor-pointer">
           <Link href="/" className="text-white no-underline group-hover:text-gray-800 group-focus:text-gray-800">
             Home
           </Link>
         </li>
-        <li className="group p-4 hover:bg-gray-200 hover:text-gray-800 focus:bg-gray-200 focus:text-gray-800 cursor-pointer">
-          <Link href="/" className="text-white no-underline group-hover:text-gray-800 group-focus:text-gray-800">
+        <li className="group p-2 hover:bg-gray-200 hover:text-gray-800 focus:bg-gray-200 focus:text-gray-800 cursor-pointer">
+          <Link href="/about" className="text-white no-underline group-hover:text-gray-800 group-focus:text-gray-800">
             About
           </Link>
         </li>
