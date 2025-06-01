bca111276b53d87feb239a26a2cbe4f5e5113266
Toteya <111172685+Toteya@users.noreply.github.com>
2025-06-01 16:56:16 +0200

    new search box functional component

--- client/src/app/components/SearchBar.js
+++ client/src/app/components/SearchBar.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const SearchBar = () => {
   return (
-    <nav>Searchbar</nav>
-  )
-}
+    <div className="flex items-center">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="m-1 p-2 rounded-md border border-gray-400 bg-slate-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
