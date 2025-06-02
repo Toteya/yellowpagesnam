1d14b9014556f3aa8c0acba3270fbb2495970269
Toteya <111172685+Toteya@users.noreply.github.com>
2025-06-02 19:03:12 +0200

    updated View to display more information about the business with icons

--- client/src/app/components/ViewPane.js
+++ client/src/app/components/ViewPane.js
@@ -1,3 +1,7 @@
+import { FaPhone } from 'react-icons/fa6';
+import { MdMail } from 'react-icons/md';
+import { FaGlobeAfrica as FaGlobe } from 'react-icons/fa';
+
 const ViewPane = ({ currentListing }) => {
   return (
     <div className="w-auto p-2">
@@ -7,9 +11,18 @@ const ViewPane = ({ currentListing }) => {
             {currentListing.name}
           </h3>
           <p className='m-2'>{currentListing.category}</p>
-          <p className='m-2'>{currentListing.email}</p>
-          <p className='m-2'>{currentListing.website}</p>
-          <p className='m-2'>{currentListing.phone_number1}</p>
+          <p className='m-2 flex items-center gap-2'>
+            <MdMail />
+            {currentListing.email}
+          </p>
+          <p className='m-2 flex items-center gap-2'>
+            <FaGlobe />
+            {currentListing.website}
+          </p>
+          <p className='m-2 flex items-center gap-2'>
+            <FaPhone />
+            {currentListing.phone_number1}
+          </p>
         </>
       ) : (
         <p className="m-2 italic text-gray-500">Select a business for more information</p>
