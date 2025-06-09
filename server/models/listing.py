ccd59c54e6627376a294c8a7aa6f4a932a6b520f
Toteya <111172685+Toteya@users.noreply.github.com>
2025-06-09 13:15:57 +0200

    added more fields to listing model: photos, likes and reviews

--- server/models/listing.py
+++ server/models/listing.py
@@ -1,7 +1,7 @@
 """
 Directory listing implementation
 """
-from mongoengine import StringField, EmailField, ListField, PointField
+from mongoengine import DictField, EmailField, IntField, ListField, PointField, StringField
 from server.models.base import BaseModel
 
 
@@ -18,6 +18,8 @@ class Listing(BaseModel):
             'website',
             'location',
             'phone_numbers',
+            'likes',
+            'reviews'
         ]
     }
 
@@ -27,3 +29,13 @@ class Listing(BaseModel):
     website = StringField(max_length=255, required=False)
     location = PointField(required=False)  # GeoJSON point for location
     phone_numbers = ListField(StringField(max_length=60), required=False)
+    photos = ListField(StringField(max_length=255), required=False)
+    likes = IntField(default=0, required=False)
+    reviews = ListField(DictField(), default=list, required=False)
+
+    def add_photo(self, filename):
+        """ Adds a photo to the listing
+        """
+        dirpath = 'photos'
+        filepath = f'{dirpath}/{self.id}/{filename}'
+        self.photos.append(filepath)
