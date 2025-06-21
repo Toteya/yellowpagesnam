40799ad00a3346140fc00ade1991867647dfd765
Toteya <111172685+Toteya@users.noreply.github.com>
2025-06-21 14:29:53 +0200

    load testing environment variables from file instead of using hard-coded variables. Improves maintainability by managing ENV variables from a centralised location

--- server/tests/__init__.py
+++ server/tests/__init__.py
@@ -1,8 +1,22 @@
 import os
 
-# Setting environment variables
-os.environ['DIRECTORY_HOST'] = 'localhost'
-os.environ['DIRECTORY_PORT'] = '27017'
-os.environ['DIRECTORY_DB'] = 'directoy_db_test'
-os.environ['DIRECTORY_ENV'] = 'testing'
-os.environ['DIRECTORY_MEDIA_DIRPATH'] = './client/src/public/media'
+# Path to the environment variables file
+env_file_path = "env_vars.txt"
+
+# Load environment variables from the file
+if os.path.exists(env_file_path):
+    os.environ['DIRECTORY_ENV'] = 'testing'
+    with open(env_file_path, "r") as env_file:
+        for line in env_file:
+            # Strip whitespace and ignore empty lines or comments
+            line = line.strip()
+            if line and not line.startswith("#"):
+                # Split the line into key and value
+                key, value = line.split("=", 1)
+                os.environ[key] = value.strip()
+
+    print("Environment variables loaded successfully:")
+else:
+    raise FileNotFoundError(
+        f"Error: Environment variables file {env_file_path} not found."
+    )
